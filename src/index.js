0dcc9bd492590ff56425e51a7dc564610907957c
Ruslan Bogdanov <57019591+XXXmez@users.noreply.github.com>
2022-01-24 15:51:58 +0300

    feat: added code project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let s = '';
+  bracketsConfig.forEach(e => {
+    e.forEach(e => s += e)
+  })
+
+  let os = [];
+
+  str.split('').forEach(e => {
+    //console.log(s.indexOf(e));
+    if (typeof e == Number || e == '|' || e == '|') {
+      //console.log(e);
+    } else if (s.indexOf(e) >= 0){
+      if (s.indexOf(e) % 2 == 1) {
+        if (s.length > 0) {
+          let last = os.splice(os.length - 1, 1).join('');
+          //console.log(last, s[s.indexOf(e) - 1]);
+          if (last != s[s.indexOf(e) - 1]){
+            return false;
+          }
+        } else {
+          return false;
+        }
+      } else {
+        os.push(e)
+      }
+    }
+  })
+  //console.log(os);
+  return os.length> 0 ? false : true;
 }
